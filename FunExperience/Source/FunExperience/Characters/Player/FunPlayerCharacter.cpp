92587694b1818b7f2df7666ae13242364b27452a
Alvys34 <51346018+Alvys34@users.noreply.github.com>
2026-02-14 16:11:53 -0500

    Start

---
+++ FunExperience/Source/FunExperience/Characters/Player/FunPlayerCharacter.cpp
@@ -0,0 +1,42 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FunPlayerCharacter.h"
+
+#include "GameFramework/SpringArmComponent.h"
+
+
+// Sets default values
+AFunPlayerCharacter::AFunPlayerCharacter()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	GetMesh()->SetupAttachment(RootComponent);
+	
+	ArmSpringComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("ArmSpringComponent"));
+	ArmSpringComponent->SetupAttachment(RootComponent);
+	ArmSpringComponent->TargetArmLength = ArmLength;
+	
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComponent"));
+	CameraComponent->SetupAttachment(ArmSpringComponent);
+	
+	
+}
+
+// Called when the game starts or when spawned
+void AFunPlayerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+// Called to bind functionality to input
+void AFunPlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	
+	
+}
+
---
+++ FunExperience/Source/FunExperience/Characters/Player/FunPlayerCharacter.h
@@ -0,0 +1,43 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/Character.h"
+#include "FunPlayerCharacter.generated.h"
+
+class USpringArmComponent;
+
+UCLASS()
+class FUNEXPERIENCE_API AFunPlayerCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AFunPlayerCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Camera")
+	TObjectPtr<USpringArmComponent> ArmSpringComponent;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Camera")
+	float ArmLength = 300.0f;
+	
+	
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Camera")
+	TObjectPtr<UCameraComponent> CameraComponent;
+
+	
+	
+	
+public:
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};
